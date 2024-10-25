396f651dd365adb9160ad6f7d92d4be1ce86579b
biyuewuhua <1669241937@qq.com>
2024-10-25 10:28:58 +0800

    commit test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+ #include  <stdio.h>
+int main(void)
+{
+    char a[100]={};
+    scanf_s("%s",&a);
+    printf("%s",a);
+    printf("%s",a);
+    printf("%s",a);
+    printf("%s",a);
+    printf("%s",a);
+    printf("%s",a);
+    return 0;
+    getchar();
+}
